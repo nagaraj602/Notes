edb9a53eee507e42ca6c8c4c4a92528976900b0d
nagaraj602 <nagarajkamath602@outlook.com>
2026-07-05 11:55:38 +0000

    Refactor EC2 instance configuration to properly structure volume settings

--- Terraform/Class 2 10-June-2026/ec2.tf
+++ Terraform/Class 2 10-June-2026/ec2.tf
@@ -1,14 +1,21 @@
 resource "aws_instance" "my_Terraform_instance" {
-  ami           = "ami-0b6d9d3d33ba97d99"
-  instance_type = "t3.micro"
-  key_name = "nagaraj"
-  security_groups = ["All traffic open"]
-  volume_size = 12
-  volume_type = "gp3"
-  volume_tags = {
-    Name = "my_Terraform_instance_volume"
+  ami             = "ami-0b6d9d3d33ba97d99"
+  instance_type   = "t3.micro"
+  key_name        = "nagaraj"
+  security_groups = ["All traffic open"] 
+
+  # The volume configurations must go inside a block device
+  root_block_device {
+    volume_size = 12
+    volume_type = "gp3"
+    
+  # Note: Inside this block, the argument is just called 'tags'
+    tags = {
+      Name = "my_Terraform_instance_volume"
+    }
   }
 
+  # These tags apply to the EC2 instance itself
   tags = {
     Name = "my_Terraform_instance_us-east-1"
   }
