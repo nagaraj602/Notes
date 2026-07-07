32f4caa24799a6cfdc9c578c0a15db0098654126
nagaraj602 <nagarajkamath602@outlook.com>
2026-07-07 11:34:29 +0000

    Fix availability zone definition for public and private subnets

--- Terraform/Class 4 13-June-2026 routes, IGW, EIP, NAT/subnet.tf
+++ Terraform/Class 4 13-June-2026 routes, IGW, EIP, NAT/subnet.tf
@@ -2,7 +2,7 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.three_tier_vpc.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone      = "us-east-1a"
 
   tags = {
     Name = "public-subnet-1"
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.three_tier_vpc.id
   cidr_block = "10.0.2.0/24"
-
+  availability_zone      = "us-east-1a"
 
   tags = {
     Name = "private-subnet-1"
