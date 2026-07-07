5412e81ac18ac90b3de196780f6804b3ccee85aa
nagaraj602 <nagarajkamath602@outlook.com>
2026-07-07 12:43:13 +0000

    Add Terraform configurations for VPC, subnets, route tables, Internet Gateway, NAT Gateway, Elastic IP, and security group

--- Terraform/Class 3 11-June-2026 input and output variable, VPC, subnet, routetable/VPC/subnet.tf
+++ Terraform/Class 3 11-June-2026 input and output variable, VPC, subnet, routetable/VPC/subnet.tf
@@ -2,7 +2,8 @@
 resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.three_tier_vpc.id
   cidr_block = "10.0.1.0/24"
-
+  availability_zone      = "us-east-1a"  
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet-1"
@@ -16,7 +17,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.three_tier_vpc.id
   cidr_block = "10.0.2.0/24"
-
+  availability_zone      = "us-east-1a"  
 
   tags = {
     Name = "private-subnet-1"
