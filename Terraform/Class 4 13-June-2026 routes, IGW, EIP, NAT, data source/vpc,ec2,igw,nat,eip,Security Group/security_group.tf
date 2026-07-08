56e93e1d692943cfd4db8fe4f7c8fcde3755d45f
nagaraj602 <nagarajkamath602@outlook.com>
2026-07-08 14:52:17 +0000

    Add Terraform configurations for VPC, security group, and EC2 instance with AMI fetching

--- Terraform/Class 4 13-June-2026 routes, IGW, EIP, NAT, data source/vpc,ec2,igw,nat,eip,Security Group/security_group.tf
+++ Terraform/Class 4 13-June-2026 routes, IGW, EIP, NAT, data source/vpc,ec2,igw,nat,eip,Security Group/security_group.tf
@@ -3,23 +3,28 @@ resource "aws_security_group" "allow_port_80" {
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.three_tier_vpc.id
 
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" # -1 stands for all traffic/protocols
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
   tags = {
     Name = "allow_port_80"
   }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_port_80.id
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_port_80.id
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"    # semantically equals to all ports and all protocols
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.allow_port_80.id
+  cidr_ipv6   = "::/0"
+  ip_protocol = "-1"    # semantically equals to all ports and all protocols
 }
