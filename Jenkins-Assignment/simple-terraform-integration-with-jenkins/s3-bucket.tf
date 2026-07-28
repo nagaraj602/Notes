f7bb4334016fabc1a5f4a44a9f552779485e48d7
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-07-29 01:12:14 +0530

    Create s3-bucket.tf

---
+++ Jenkins-Assignment/simple-terraform-integration-with-jenkins/s3-bucket.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "remote_backend_s3" {
+  bucket        = "remote-backend-s3-jan2026-Nagaraj"
+  force_destroy = true    # only if you're planning to delete bucket in future
+  use_lockfile = true
+}
+
+
+resource "aws_s3_bucket_versioning" "versioning_bucket" {
+  bucket = aws_s3_bucket.remote_backend_s3-jan2026-Nagaraj.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
