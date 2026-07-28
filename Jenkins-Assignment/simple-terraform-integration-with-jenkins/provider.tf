645782e611dfc01145a677b0ed7b1b427d065945
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-07-29 01:13:59 +0530

    Update provider.tf

--- Jenkins-Assignment/simple-terraform-integration-with-jenkins/provider.tf
+++ Jenkins-Assignment/simple-terraform-integration-with-jenkins/provider.tf
@@ -15,6 +15,7 @@ backend "s3" {
   bucket = "remote-backend-s3-jan2026-Nagaraj"
   key    = "simple-terraform-Jenkins-pipeline/terraform.tfstate"
   region = "us-east-1"
+  use_lockfile = true
 }
 
 
