eca7423acdc7aba212a308a810c1fed1a038ae28
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-07-28 19:59:01 +0530

    Update route_table.tf

--- Jenkins-Assignment/simple-terraform-integration-with-jenkins/route_table.tf
+++ Jenkins-Assignment/simple-terraform-integration-with-jenkins/route_table.tf
@@ -18,17 +18,22 @@ resource "aws_route_table_association" "public_rta" {
 }
 
 
-# Create Private Route Table
+# Creating Private Route Table
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.three_tier_vpc.id
- 
+
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
   tags = {
     Name = "private-route-table"
   }
 }
 
 
-# Associate Route Table with Private Subnet
+# Route Table Association for Private Subnet
 resource "aws_route_table_association" "private_rta" {
   subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_route_table.id
